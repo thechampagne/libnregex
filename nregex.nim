d0d2b26ef917498c86483a68160da028d8c7518b
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-05-27 14:15:21 +0300

    create TODOs section

--- nregex.nim
+++ nregex.nim
@@ -20,3 +20,25 @@ proc nre_is_match(s: cstring, pattern: cstring): c_int {.exportc.} =
   else:
     return 0
 
+
+# TODOs:
+# func group(m: RegexMatch; i: int; text: string): seq[string]
+# func groupFirstCapture(m: RegexMatch; i: int; text: string): string
+# func groupLastCapture(m: RegexMatch; i: int; text: string): string
+# func group(m: RegexMatch; groupName: string; text: string): seq[string]
+# func groupFirstCapture(m: RegexMatch; groupName: string; text: string): string
+# func groupLastCapture(m: RegexMatch; groupName: string; text: string): string
+# func groupsCount(m: RegexMatch): int
+# func groupNames(m: RegexMatch): seq[string
+# func findAndCaptureAll(s: string; pattern: Regex): seq[string]
+# func contains(s: string; pattern: Regex): bool
+# func find(s: string; pattern: Regex; m: var RegexMatch; start = 0): bool
+# func splitIncl(s: string; sep: Regex): seq[string]
+# func startsWith(s: string; pattern: Regex; start = 0): bool
+# func endsWith(s: string; pattern: Regex): bool
+# func replace(s: string; pattern: Regex; by: string; limit = 0): string
+# iterator group(m: RegexMatch; i: int): Slice[int]
+# iterator group(m: RegexMatch; s: string): Slice[int]
+# iterator findAll(s: string; pattern: Regex; start = 0): RegexMatch
+# iterator findAllBounds(s: string; pattern: Regex; start = 0): Slice[int]
+# iterator split(s: string; sep: Regex): string
