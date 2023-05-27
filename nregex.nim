c977cd5699cf7f09bca27c7722589c85ce45875e
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-05-27 14:08:40 +0300

    create init function for nre_regex_match_t

--- nregex.nim
+++ nregex.nim
@@ -3,6 +3,10 @@ import regex
 type
   nre_regex_match_t = object
 
+proc nre_regex_match_init(): ptr nre_regex_match_t {.exportc.} =
+  let rm = create RegexMatch
+  return cast[ptr nre_regex_match_t](rm)
+
 proc nre_match(s: cstring, pattern: cstring, m: ptr nre_regex_match_t): c_int {.exportc.} =
   var rm = cast[ptr RegexMatch](m)
   if match($s, re($pattern), rm[]):
