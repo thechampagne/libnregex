a3ea0c56a162179a2d90c6e900fbbdfaac53d98f
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-06-05 14:13:16 +0300

    add groupsCount, groupNames, replace, and splitIncl

--- nregex.nim
+++ nregex.nim
@@ -57,6 +57,45 @@ proc nre_find(s: cstring, pattern: ptr nre_regex_t, m: ptr nre_regex_match_t, st
   else:
     return 0
 
+proc nre_regex_match_groups_count(m: ptr nre_regex_match_t): cint =
+  let rm = cast[ptr RegexMatch](m)
+  return cint(groupsCount(rm[]))
+
+proc nre_regex_match_group_names(m: ptr nre_regex_match_t): ptr ptr cchar =
+  let rm = cast[ptr RegexMatch](m)
+  let names = groupNames(rm[])
+  if names.len == 0:
+    return nil
+  var carray = cast[ptr ptr cchar](alloc((sizeof(ptr cchar)) * names.len))
+  for i,v in names:
+    var cstr = cast[ptr cchar](alloc((sizeof cchar) * v.len + 1))
+    copyMem(cstr, cast[pointer](unsafeAddr v), v.len)
+    cast[ptr UncheckedArray[cchar]](cstr)[v.len] = '\0'
+    cast[ptr UncheckedArray[ptr cchar]](carray)[i] = cstr
+  return carray
+
+proc nre_replace(s: cstring, pattern: ptr nre_regex_t, by: cstring, limit: csize_t): ptr cchar =
+  let r = cast[ptr Regex](pattern)
+  let replace = replace($s, r[], $by, int(limit))
+  var cstr = cast[ptr cchar](alloc((sizeof cchar) * replace.len + 1))
+  copyMem(cstr, cast[pointer](unsafeAddr replace), replace.len)
+  cast[ptr UncheckedArray[cchar]](cstr)[replace.len] = '\0'
+  return cstr
+
+proc nre_split_incl(s: cstring, sep: ptr nre_regex_t): ptr ptr cchar =
+  let r = cast[ptr Regex](sep)
+  let splits = splitIncl($s, r[])
+  if splits.len == 0:
+    return nil
+  var carray = cast[ptr ptr cchar](alloc((sizeof(ptr cchar)) * splits.len))
+  for i,v in splits:
+    var cstr = cast[ptr cchar](alloc((sizeof cchar) * v.len + 1))
+    copyMem(cstr, cast[pointer](unsafeAddr v), v.len)
+    cast[ptr UncheckedArray[cchar]](cstr)[v.len] = '\0'
+    cast[ptr UncheckedArray[ptr cchar]](carray)[i] = cstr
+  return carray
+
+
 
 # TODOs:
 # func group(m: RegexMatch; i: int; text: string): seq[string]
@@ -65,11 +104,7 @@ proc nre_find(s: cstring, pattern: ptr nre_regex_t, m: ptr nre_regex_match_t, st
 # func group(m: RegexMatch; groupName: string; text: string): seq[string]
 # func groupFirstCapture(m: RegexMatch; groupName: string; text: string): string
 # func groupLastCapture(m: RegexMatch; groupName: string; text: string): string
-# func groupsCount(m: RegexMatch): int
-# func groupNames(m: RegexMatch): seq[string
 # func findAndCaptureAll(s: string; pattern: Regex): seq[string]
-# func splitIncl(s: string; sep: Regex): seq[string]
-# func replace(s: string; pattern: Regex; by: string; limit = 0): string
 # iterator group(m: RegexMatch; i: int): Slice[int]
 # iterator group(m: RegexMatch; s: string): Slice[int]
 # iterator findAll(s: string; pattern: Regex; start = 0): RegexMatch
